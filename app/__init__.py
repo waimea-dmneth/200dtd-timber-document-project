1138e000d78546c2f43dfc9b602c7f9860408b17
waimea-dmneth <dmneth@waimea.school.nz>
2025-08-26 13:25:30 +1200

    profile image working

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@
 # BRIEF DESCRIPTION OF YOUR PROJECT HERE
 #===========================================================
 
-from flask import Flask, render_template, request, flash, redirect, send_file
+from flask import Flask, render_template, request, flash, redirect, send_file, jsonify
 import html
-import io
+import io, base64
 
 from app.helpers.session import init_session
 from app.helpers.db      import connect_db
@@ -25,14 +25,23 @@ init_logging(app)   # Log requests
 init_error(app)     # Handle errors and exceptions
 init_datetime(app)  # Handle UTC dates in timestamps
 
-@app.get("/profile_image/<int:profile_id>")
-def profile_image(profile_id):
+#-----------------------------------------------------------
+# Send Img
+#-----------------------------------------------------------
+
+@app.get("/profile_image/<string:profile>")
+def profile_image(profile):
     with connect_db() as client:
-        sql = "SELECT pic FROM Profile WHERE profile = %s"
-        result = client.execute(sql, [profile_id]).fetchone()
+        sql = "SELECT pic FROM Profile WHERE profile = ?"
+        result = client.execute(sql, [profile])  # already a list of dict-like rows
+
         if result:
-            return send_file(io.BytesIO(result['pic']), mimetype='image/png')
-        return "Not Found", 404
+            row = result[0]           # first row
+            pic_data = row["pic"]     # get the blob
+            img_str = base64.b64encode(pic_data).decode("utf-8")
+            return jsonify({"image": f"data:image/png;base64,{img_str}"})
+        
+        return not_found_error()
 
 
 #-----------------------------------------------------------
@@ -42,20 +51,10 @@ def profile_image(profile_id):
 def index():
     with connect_db() as client:
         # Get all the things from the DB
-        sql = "SELECT * FROM Profile"
+        sql = "SELECT profile FROM Profile"
         params = []
         results = client.execute(sql, params).rows
-
-        profiles = []
-        for row in results:
-            img_data = io.BytesIO(row['pic'])
-            profiles.append({
-                'profile': row['profile'],
-                'pic': img_data
-            })
-        print(profiles)
-
-        return render_template("pages/home.jinja", profiles = profiles)
+        return render_template("pages/home.jinja", profiles = results)
 
 
 #-----------------------------------------------------------
@@ -114,39 +113,39 @@ def show_all_things():
 #-----------------------------------------------------------
 # Route for adding a thing, using data posted from a form
 #-----------------------------------------------------------
-@app.post("/add")
-def add_a_thing():
-    # Get the data from the form
-    name  = request.form.get("name")
-    price = request.form.get("price")
+# @app.post("/add")
+# def add_a_thing():
+#     # Get the data from the form
+#     name  = request.form.get("name")
+#     price = request.form.get("price")
 
-    # Sanitise the text inputs
-    name = html.escape(name)
+#     # Sanitise the text inputs
+#     name = html.escape(name)
 
-    with connect_db() as client:
-        # Add the thing to the DB
-        sql = "INSERT INTO things (name, price) VALUES (?, ?)"
-        params = [name, price]
-        client.execute(sql, params)
+#     with connect_db() as client:
+#         # Add the thing to the DB
+#         sql = "INSERT INTO things (name, price) VALUES (?, ?)"
+#         params = [name, price]
+#         client.execute(sql, params)
 
-        # Go back to the home page
-        flash(f"Thing '{name}' added", "success")
-        return redirect("/things")
+#         # Go back to the home page
+#         flash(f"Thing '{name}' added", "success")
+#         return redirect("/things")
 
 
-#-----------------------------------------------------------
-# Route for deleting a thing, Id given in the route
-#-----------------------------------------------------------
-@app.get("/delete/<int:id>")
-def delete_a_thing(id):
-    with connect_db() as client:
-        # Delete the thing from the DB
-        sql = "DELETE FROM things WHERE id=?"
-        params = [id]
-        client.execute(sql, params)
+# #-----------------------------------------------------------
+# # Route for deleting a thing, Id given in the route
+# #-----------------------------------------------------------
+# @app.get("/delete/<int:id>")
+# def delete_a_thing(id):
+#     with connect_db() as client:
+#         # Delete the thing from the DB
+#         sql = "DELETE FROM things WHERE id=?"
+#         params = [id]
+#         client.execute(sql, params)
 
-        # Go back to the home page
-        flash("Thing deleted", "success")
-        return redirect("/things")
+#         # Go back to the home page
+#         flash("Thing deleted", "success")
+#         return redirect("/things")
 
 
